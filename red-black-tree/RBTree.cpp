8a3163a8186a072f5fd3da1b4ceb8646ded8378c
George Georgiev <gtqnchev@gmail.com>
2013-02-06 17:10:42 +0200

    added a print function and a sketch for insert

--- red-black-tree/RBTree.cpp
+++ red-black-tree/RBTree.cpp
@@ -8,8 +8,17 @@ struct RBNode
     bool color;
     RBNode<T> *left;
     RBNode<T> *right;
-    RBNode(T v, bool c, RBNode *l = NULL, RBNode *r = NULL): value(v), color(v), left(l), right(r)
+    RBNode(T v, bool c = 1, RBNode *l = NULL, RBNode *r = NULL): value(v), color(v), left(l), right(r)
     {}
+    
+    void Print(int offset)
+	{
+		if(right) right->Print(offset + 5);
+        
+        cout << setw(offset) << value << endl;
+		
+        if(left) left->Print(offset + 5);
+	}
 };
 
 template <typename T = int>
@@ -28,15 +37,40 @@ public:
     {
         return Insert(value, root);
     }
+    
+    void Print()
+    {
+        root->Print(0);
+    }
+}
 private:
-    bool Insert(T value, Node*
-};
+    bool Insert(T value, Node* node)
+    {
+        if (node == NULL)
+        {
+            node = new RBNode<T>(value);
+            return true;
+        }
+        else if (node->value > value)
+            return Insert(value, node->left);
+        
+        else if (node->value < value)
+            return Insert(value, node->right);
+        
+        return false;
+};  
 
 int main ()
 {
     cout << "Testing a red-black tree." << endl;
     RBTree<> tree;
+    
     cout << "Test if a new Tree is empty:" << tree.Empty() << endl;
     
+    cout << "Test if we can add elements:" << tree.Add(5) << endl;
+    
+    cout << "Test if we can print the tree:" <<endl;
+        tree.Print();
+        
     return 0;
 }
